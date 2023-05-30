00115c990bb930c6a5f5283054026314a37c747a
Andreas <me@apap04.com>
2023-05-29 22:27:11 -0400

    chore: add .env.prod to gitignore

--- app/(dashboard)/d/products/page.tsx
+++ app/(dashboard)/d/products/page.tsx
@@ -42,6 +42,7 @@ export default function ProductsPage() {
     "https://discord.com/oauth2/authorize?permissions=8&guild_id=1108375792393662474&response_type=code&redirect_uri=https%3A%2F%2Fapi.hyper.co%2Fauth%2Flogin%2Fdiscord%2Fcallback&scope=bot&state=%7B%22redirect%22%3A%22%2Fproducts%2Fnew%3Frecipe%3Ddiscord%26integrations%5Bdiscord%5D%5Bguild%5D%3D1108375792393662474%22%7D&client_id=648234176805470248";
 
   const [selectedServer, setSelectedServer] = useState<string>();
+  
   const [selectedRole, setSelectedRole] = useState<string>();
   const [paypathRolePos, setPaypathRolePos] = useState<number>(8);
 
